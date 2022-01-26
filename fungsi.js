b9525bcb5fbbe1a064da886ce0679076c14b32ad
FajarAriyanto447 <fajarariyanto447@gmail.com>
2022-01-26 15:51:27 +0700

    belajar-javascript-2

---
+++ fungsi.js
@@ -0,0 +1,12 @@
+// function sayHello() {
+//     console.log(12 + 70);
+// }
+
+// sayHello()
+
+document.write("<ol>");
+
+for (let i = 0; i < product.lenght; i++) {
+    document.write(`<li>${product[i] }</li>`);
+}
+document.write("</lo>");
